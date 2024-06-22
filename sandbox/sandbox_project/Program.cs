556b9a453a499e65b9381f737b3dbda7411b96cf
Michaelsoft-Riley <ril22002@byui.edu>
2024-06-21 21:50:25 -0600

    testing

--- sandbox/sandbox_project/Program.cs
+++ sandbox/sandbox_project/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics;
 
 public class Program
 {
@@ -9,5 +10,8 @@ public class Program
         // one of your projects.
 
         Console.WriteLine("Hello Sandbox World!");
+        var x = 1;
+        var y = 2;
+        Trace.Assert(x == y, "That aint right");
     }
 }
