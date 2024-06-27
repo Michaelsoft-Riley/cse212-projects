18e8faf1122d1fa805f3985162a7ec364b79f104
Michaelsoft-Riley <ril22002@byui.edu>
2024-06-26 20:50:37 -0600

    Practicing with lambdas and sorting

--- sandbox/sandbox_project/Program.cs
+++ sandbox/sandbox_project/Program.cs
@@ -9,9 +9,20 @@ public class Program
         // with any code or ideas you have that do not directly apply to
         // one of your projects.
 
-        Console.WriteLine("Hello Sandbox World!");
-        var x = 1;
-        var y = 2;
-        Trace.Assert(x == y, "That aint right");
+        var numberDict = new Dictionary<string, int>
+        {
+            {"two", 2},
+            {"one", 1},
+            {"three", 3},
+            {"five", 5},
+            {"four", 4}
+        };
+        // var numbers = numberDict.ToArray();
+        // Array.Sort(numbers, (p1, p2) => p1.Value - p2.Value);
+        var numbers = numberDict.OrderByDescending(n => n.Value).ToArray();
+
+        foreach (var number in numbers) {
+            Console.WriteLine(number);
+        }
     }
 }
