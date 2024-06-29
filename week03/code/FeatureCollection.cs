9e717af398280cc0fdecd275488a2debb3ff2744
Michaelsoft-Riley <ril22002@byui.edu>
2024-06-29 17:46:50 -0600

    Added classes and attributes for JSON deserialization

--- week03/code/FeatureCollection.cs
+++ week03/code/FeatureCollection.cs
@@ -1,4 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+
 public class FeatureCollection {
     // Todo Earthquake Problem - ADD YOUR CODE HERE
     // Create additional classes as necessary
+    public List<Feature> Features { get; set; }
+    public void Display() {
+        foreach (var feature in Features) {
+            feature.properties.Display();
+        }
+    }
+}
+
+public class Feature {
+    public Properties properties { get; set; }
+}
+
+public class Properties {
+    public string Place { get; set; }
+    public double Mag { get; set; }
+
+    public void Display() {
+        Console.WriteLine($"{Place} - Mag {Mag}");
+    }
 }
